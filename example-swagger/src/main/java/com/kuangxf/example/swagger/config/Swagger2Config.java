8c6fc452cbccb99b1a9bd8559cd71c2c586eb032
PC-20180202FXRO\Administrator <497446610@qq.com>
2018-02-03 13:02:56 +0800

    调整contact

--- example-swagger/src/main/java/com/kuangxf/example/swagger/config/Swagger2Config.java
+++ example-swagger/src/main/java/com/kuangxf/example/swagger/config/Swagger2Config.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,9 +23,11 @@ public class Swagger2Config {
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("Spring Boot中使用Swagger2构建RESTful APIs")
-				.description("更多Spring Boot相关文章请关注：http://www.kuangxf.com/")
-				.termsOfServiceUrl("http://www.kuangxf.com/").contact("况小锋").version("1.0").build();
+		return new ApiInfoBuilder().title("Spring Boot中使用Swagger2构建RESTful APIs")//
+				.description("更多Spring Boot相关文章请关注：http://www.kuangxf.com/")//
+				.termsOfServiceUrl("http://www.kuangxf.com/")//
+				.contact(new Contact("况小锋", "http://www.kuangxf.com/", "497446610@qq.com"))//
+				.version("1.0").build();
 	}
 
 }
